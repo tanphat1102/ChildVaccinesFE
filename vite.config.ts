c1c19317a0d79398255f19961b26729525ad3f29
Tan Phat Tran <chopml1102@gmail.com>
2025-04-23 22:44:48 +0700

    fix to deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "./",
-  server: {
-    port: 7979,
-    open: true, 
-  }
+  // server: {
+  //   port: 7979,
+  //   open: true, 
+  // }
 })
